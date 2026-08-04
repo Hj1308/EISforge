f9dc845f878632a3529bb5d8b3439ae1626210af
HodaJafari <hoda.jaafari@gmail.com>
2026-08-04 21:58:14 +0100

    chore: untrack one-off patch scripts and backup files

--- patch20_band_edge.py
+++
@@ -1,6 +0,0 @@
-# patch20_band_edge.py
-# This patch was applied automatically by EISForge CI.
-# Files added:
-#   eisforge/analysis/band_edge_calculator.py
-#   tests/test_band_edge.py
-# See commit message for details.
